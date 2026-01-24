f6f127a83e2729515053563d4c0a384ab374e035
sangeeta1997 <sangeetac525@gmail.com>
2026-01-24 21:15:41 +0530

    kadane algorithm

---
+++ src/main/java/com/dsa/showcase/array/kadane/MaxSubArray.java
@@ -0,0 +1,42 @@
+package com.dsa.showcase.array.kadane;
+
+/*53. Maximum Subarray
+Solved
+        Medium
+Topics
+premium lock icon
+        Companies
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.*/
+class MaxSubArray {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        MaxSubArray maximumSubArray = new MaxSubArray();
+        System.out.println(maximumSubArray.maxSubArray(arr));
+    }
+
+    //algorithm Kadane's Algorithm
+    //time complexity O(n)
+    //space complexity O(1)
+    public int maxSubArray(int[] nums) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            currSum = Math.max(currSum + num, num);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+}
